de9dba681b2576a0065829ebb406679a6fc38f97
Mailer-999 <ybmycgkfcg@privaterelay.appleid.com>
2026-09-10 18:20:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const ตรงนี้คือไรให้ทาย = document.getElementById("loginButton");
-loginButton.addEventListener("click", function(){alert("ขอเช็คก่อนน้า");});
+const employeeID = document.getElementById("รหัสพนง.");
+const department = document.getElementById("แผนก");
+const password = document.getElementById("รหัส");
+const loginButton = document.getElementById("loginButton");
+
+loginButton.addEventListener("click", function() {
+    alert(
+        "รหัสพนักงาน: " + employeeID.value +
+        "\nแผนก: " + department.value
+    );
+});
