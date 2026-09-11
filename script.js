c74ad4773e9d0f419cb173b22f19c8f6c2373904
Mailer-999 <ybmycgkfcg@privaterelay.appleid.com>
2026-09-11 08:21:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+const correctEmployeeID = "224236";
+const correctPassword = "01234";
 const employeeID = document.getElementById("รหัสพนง.");
 const department = document.getElementById("แผนก");
 const password = document.getElementById("รหัส");
 const loginButton = document.getElementById("loginButton");
 
 loginButton.addEventListener("click", function() {
-    alert(
-        "รหัสพนักงาน: " + employeeID.value +
-        "\nแผนก: " + department.value
-    );
+
+    if (employeeID.value === "") {
+        alert("กรุณากรอกรหัสพนักงาน");
+        return;
+    }
+
+    if (department.value === "") {
+        alert("กรุณากรอกแผนก");
+        return;
+    }
+
+    if (password.value === "") {
+        alert("กรุณากรอกรหัสผ่าน");
+        return;
+    }
+
+    if (employeeID.value !== correctEmployeeID) {
+    alert("รหัสพนักงานไม่ถูกต้อง");
+    return;
+}
+if (password.value !== correctPassword) {
+    alert("รหัสผ่านไม่ถูกต้อง");
+    return;
+}
 });
